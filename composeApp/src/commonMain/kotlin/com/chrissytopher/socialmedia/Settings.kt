172aad019b28ad8d8c156114400d3679999e2746
Irmlol <Hiram.Helliwell@gmail.com>
2025-03-20 13:56:47 -0700

    made setting text proper for dark mode

--- composeApp/src/commonMain/kotlin/com/chrissytopher/socialmedia/Settings.kt
+++ composeApp/src/commonMain/kotlin/com/chrissytopher/socialmedia/Settings.kt
@@ -30,7 +30,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.chrissytopher.socialmedia.theme.darkScheme
 
 val likeIcons = listOf(
     Pair(Icons.Outlined.FavoriteBorder, Icons.Outlined.Favorite),
@@ -44,7 +46,7 @@ fun Settings (viewModel: AppViewModel) {
     val quag by viewModel.quag
     val darkMode by viewModel.darkMode
     Column {
-        Text("Like Icon:", Modifier.padding(10.dp, 0.dp), style = MaterialTheme.typography.titleLarge)
+        Text("Like Icon:", Modifier.padding(10.dp, 0.dp), style = MaterialTheme.typography.titleLarge, color = MaterialTheme.colorScheme.onSurface)
         Row {
             for (i in 0..2) {
                 ElevatedFilterChip(
@@ -76,13 +78,13 @@ fun Settings (viewModel: AppViewModel) {
 }
 
 @Composable
-fun SettingToggle(type: Int, Key: String, setting: Boolean, func: ()-> Unit) {
+fun SettingToggle(type: Int, key: String, setting: Boolean, func: ()-> Unit) {
     val typeModifier = when (type) {
         0 -> Modifier.fillMaxWidth()
         else -> Modifier
     }
     Row(Modifier.padding(5.dp).height(30.dp).then(typeModifier)) {
-        Text(Key,Modifier.align(Alignment.CenterVertically), style = MaterialTheme.typography.titleLarge)
+        Text(key,Modifier.align(Alignment.CenterVertically), style = MaterialTheme.typography.titleLarge, color = MaterialTheme.colorScheme.onSurface)
         Switch(checked = setting, onCheckedChange = { func() })
     }
 }
