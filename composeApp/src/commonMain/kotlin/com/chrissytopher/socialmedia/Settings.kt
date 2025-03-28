bb1d0b47848bbbb9a09ebb2234b6932d6029683e
Irmlol <Hiram.Helliwell@gmail.com>
2025-03-28 14:03:01 -0700

    made settings formating work finaly party time!!!

--- composeApp/src/commonMain/kotlin/com/chrissytopher/socialmedia/Settings.kt
+++ composeApp/src/commonMain/kotlin/com/chrissytopher/socialmedia/Settings.kt
@@ -1,9 +1,12 @@
 package com.chrissytopher.socialmedia
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,17 +19,29 @@ import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.Forward
 import androidx.compose.material.icons.outlined.ThumbUp
+import androidx.compose.material.icons.outlined.ToggleOff
+import androidx.compose.material.icons.outlined.ToggleOn
 import androidx.compose.material.icons.sharp.Forward
 import androidx.compose.material.icons.sharp.ThumbUp
+import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.ElevatedFilterChip
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.chrissytopher.socialmedia.theme.darkScheme
 
 val likeIcons = listOf(
     Pair(Icons.Outlined.FavoriteBorder, Icons.Outlined.Favorite),
@@ -75,8 +90,10 @@ fun Settings (viewModel: AppViewModel) {
                 viewModel.setDarkMode(darkMode?.not() ?: !systemDarkTheme)
             }
 
-            settingToggle(settingFormat, "Setting format", (settingFormat == 1)) {
-                viewModel.setSettingFormat(if (settingFormat == 1) 2 else 1)
+            Button(onClick = {
+                viewModel.setSettingFormat(0)
+            }) {
+
             }
 
         }
@@ -101,22 +118,48 @@ fun settingToggle(type: Int, key: String, setting: Boolean, func: () -> Unit) {
 
 @Composable
 fun settingFormatPicker(viewModel: AppViewModel) {
-    viewModel.setSettingFormat(1)
-//    Column {
-//        Text("Select A format", style = MaterialTheme.typography.titleLarge, color = MaterialTheme.colorScheme.onSurface, modifier = Modifier.padding(20.dp))
-//        Row {
-//            Card(Modifier.weight(1f).padding(5.dp)) {
-//                Column {
-//                    for (n in 1..3) {
-//                        Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-//                            Text("setting ")
-//                            Icon(Icons.Outlined.ToggleOn, null)
-//                        }
-//                    }
-//                }
-//            }
-//            Card(Modifier.weight(1f).padding(5.dp)) {
-//                Column {
+    var selectedMode by remember { mutableStateOf(0) }
+    var done by remember { mutableStateOf(false)}
+    if(!done) {
+        Column {
+            Text(
+                "Select a format",
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.onSurface,
+                modifier = Modifier.padding(20.dp)
+            )
+            Row {
+                for (i in 1..2) {
+                    val typeModifier =
+                        when (i) {
+                            1 -> Pair(Modifier.fillMaxWidth(), Arrangement.SpaceBetween)
+                            else -> Pair(Modifier, Arrangement.Start)
+                        }
+                    Card(
+                        Modifier.weight(1f).padding(5.dp).clip(MaterialTheme.shapes.medium).border(
+                            width = 2.dp,
+                            color = if (selectedMode == i) Color.Magenta else Color.Black,
+                            shape = MaterialTheme.shapes.medium
+                        ).clickable(selectedMode != i) {
+                            selectedMode = i
+                        }) {
+                        Column(Modifier.padding(5.dp)) {
+                            for (n in 1..3) {
+                                Row(
+                                    Modifier.then(typeModifier.first),
+                                    horizontalArrangement = typeModifier.second
+                                ) {
+                                    Text(if (i == 1) "setting " else "setting" + "g".repeat(n % 3))
+                                    Icon(Icons.Outlined.ToggleOn, null)
+                                }
+                            }
+                        }
+                    }
+                }
+//            Card(Modifier.weight(1f).padding(5.dp).clip(MaterialTheme.shapes.medium).border(width = 2.dp, color = if (selectedMode == 2) Color.Magenta else Color.Black, shape = MaterialTheme.shapes.medium).clickable(selectedMode != 2) {
+//                selectedMode = 2
+//            }, elevation = CardDefaults.elevatedCardElevation(3.dp, 0.dp, 1.dp, 1.dp, 1.dp, 3.dp)) {
+//                Column(Modifier.padding(5.dp)) {
 //                    for (n in 1..3) {
 //                        Row() {
 //                            Text("setting " + "n".repeat(n%3))
@@ -125,7 +168,14 @@ fun settingFormatPicker(viewModel: AppViewModel) {
 //                    }
 //                }
 //            }
-//        }
-//    }
+            }
+            Button(onClick = {
+                done = true
+            }, enabled = selectedMode != 0) {
+                Text("Done")
+            }
+        }
+    } else {
+        Settings(viewModel)
+    }
 }
-
